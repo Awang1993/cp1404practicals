4ebabf8369614d676c55de5b3425ef7541379587
Adrian <adrian.wang@my.jcu.edu.au>
2020-08-27 20:57:02 +1000

    "Added score check function"

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -3,13 +3,22 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score <= 0 or score >= 100:
-    print("Invalid score")
+
+def main():
     score = float(input("Enter score: "))
-if score < 50:
-    print("Bad")
-elif score > 91:
-    print("Excellent")
-else:
-    print("Passable")
+    while score <= 0 or score >= 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    print(score_check(score))
+
+
+def score_check(score):
+    if score < 50:
+        return "Bad"
+    elif score > 91:
+        return "Excellent"
+    else:
+        return "Passable"
+
+
+main()
