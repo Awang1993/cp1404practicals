7a46cc7f4cc8695f449ce9aa730493ca3bbb5bb0
Adrian <adrian.wang@my.jcu.edu.au>
2020-08-31 19:44:29 +1000

    "Answered questions for warmup exercise"

---
+++ Prac_04/lists_warmup.py
@@ -0,0 +1,24 @@
+"""
+CP1404 - Prac 04
+"""
+
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+#
+# numbers[0] = prints first value in list = 3
+# numbers[-1] = prints last value in list = 2
+# numbers[3] = prints fourth value in list = 1
+# numbers[:-1] = prints list UP TO last value = 3, 1, 4, 1, 5, 9
+# numbers[3:4] = prints from fourth value to fifth = 1
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False, because 3 queried as a string
+# numbers + [6, 5, 3] = appends list at the end of 'numbers' list
+
+# 1. Change the first element of numbers to "ten"
+#       numbers[0] = "ten"
+# 2. Change the last element of numbers to 1
+#       numbers[-1] = 1
+# 3. Get all elements from numbers except the first two (slice)
+#       numbers[2:]
+# 4. Check if 9 is an element of numbers
+#       9 in numbers
