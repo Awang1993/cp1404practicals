424b60c927a10e01cc5c60015059c540bcfa9218
Adrian <adrian.wang@my.jcu.edu.au>
2020-08-31 20:56:09 +1000

    "Applied 'Things to do' activities to raw code"

--- Prac_04/total_income.py
+++ Prac_04/total_income.py
@@ -8,18 +8,22 @@ def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
     i = int(input("How many months? "))
-    months = i
+    number_of_months = i
 
-    for month in range(1, months + 1):
-        income = float(input("Enter income for month " + str(month) + ": "))
+    for month in range(1, number_of_months + 1):
+        income = float(input("Enter income for month {}: ".format(str(month))))
         incomes.append(income)
 
+    generate_income_report(number_of_months, incomes)
+
+
+def generate_income_report(number_of_months, incomes):
     print("\nIncome Report\n-------------")
     total = 0
-    for month in range(1, months + 1):
+    for month in range(1, number_of_months + 1):
         income = incomes[month - 1]
         total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+        print("Month {:2} - Income: ${:^13.2f} Total: ${:>10.2f}".format(month, income, total))
 
 
 main()
