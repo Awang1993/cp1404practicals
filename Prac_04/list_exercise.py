07cea29b8e5e0f69920face7be91e49e6cfff271
Adrian <adrian.wang@my.jcu.edu.au>
2020-09-01 15:47:33 +1000

    "Added basic list operations and security check programs"

--- Prac_04/list_exercise.py
+++ Prac_04/list_exercise.py
@@ -2,14 +2,25 @@
 CP1404 - Basic List Operations
 """
 
-numbers = []
+# numbers = []
+#
+# print("Please enter 5 numbers")
+# for i in range(5):
+#     user_number = int(input("Number:"))
+#     number = numbers.append(user_number)
+# print("The first number is {}".format(numbers[0]))
+# print("The last number is {}".format(numbers[-1]))
+# print("The smallest number is {}".format(min(numbers)))
+# print("The largest number is {}".format(max(numbers)))
+# print("The average of the numbers is {}".format((sum(numbers)/len(numbers))))
 
-print("Please enter 5 numbers")
-for i in range(5):
-    user_number = int(input("Number:"))
-    number = numbers.append(user_number)
-print("The first number is {}".format(numbers[0]))
-print("The last number is {}".format(numbers[-1]))
-print("The smallest number is {}".format(min(numbers)))
-print("The largest number is {}".format(max(numbers)))
-print("The average of the numbers is {}".format((sum(numbers)/len(numbers))))
+"""Woefully inadequate security checker"""
+
+# usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+#              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+#
+# username = input("Username: ")
+# while username not in usernames:
+#     print("Access denied")
+#     username = input("Username: ")
+# print("Access granted")
