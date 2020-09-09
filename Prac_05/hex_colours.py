9ae9ba23f37394cfc7f1828507241c0a555a0992
Adrian <adrian.wang@my.jcu.edu.au>
2020-09-09 20:34:48 +1000

    "Added loop for program to print key and value of selected colour"

--- Prac_05/hex_colours.py
+++ Prac_05/hex_colours.py
@@ -7,11 +7,11 @@ HEX_COLOURS = {"aliceblue": "#ff0f8ff", "beige": "#f5f5dc", "black": "#000000", 
                "blueviolet": "#8a2be2", "brown": "#a52a2a", "chocolate": "#d2691e", "coral": "#ff7f50",
                "cornflowerblue": "#6495ed", "darkgreen": "#006400"}
 
-print(HEX_COLOURS.keys())
-user_colour = input("Please choose a colour: ").lower()
-while user_colour != "":
-    if user_colour in HEX_COLOURS:
-        print(user_colour, "is", HEX_COLOURS[user_colour])
-    else:
-        print("Sorry, the colour you chose is not listed.")
-    user_colour = input("Please choose a colour: ").lower()
+# print(HEX_COLOURS.keys())
+# user_colour = input("Please choose a colour: ").lower()
+# while user_colour != "":
+#     if user_colour in HEX_COLOURS:
+#         print(user_colour, "is", HEX_COLOURS[user_colour])
+#     else:
+#         print("Sorry, the colour you chose is not listed.")
+#     user_colour = input("Please choose a colour: ").lower()
