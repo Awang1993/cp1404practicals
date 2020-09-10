cff2ae6f4e1ca09b3d86cd6dbd3ec237201c3108
Adrian <adrian.wang@my.jcu.edu.au>
2020-09-10 18:59:33 +1000

    "Added name check function"

--- Prac_05/emails.py
+++ Prac_05/emails.py
@@ -16,9 +16,9 @@ def main():
         else:
             fullname = user_name[0].title()
         name_check = input("Is your name {}? (y/n) ".format(fullname)).lower()
-        if name_check == 'n':
-            new_name = input("Name: ")
-            EMAILS[new_name.title()] = user_email
+        # if name_check == 'n':
+        #     new_name = input("Name: ")
+        #     EMAILS[new_name.title()] = user_email
         else:
             EMAILS[fullname.title()] = user_email
         user_email = input("Email: ")
