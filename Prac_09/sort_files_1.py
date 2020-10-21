25f746449c0fa77d13567fff410f8ed80614964e
Adrian <adrian.wang@my.jcu.edu.au>
2020-10-21 13:43:41 +1000

    "Included error handling and conditions if errors occur."

--- Prac_09/sort_files_1.py
+++ Prac_09/sort_files_1.py
@@ -9,28 +9,21 @@ import shutil
 
 def main():
     """Main program for sorting files"""
-    extensions = []
 
     os.chdir('FilesToSort')
 
-    print(os.listdir('.'))  # MARKER
-    print(extensions)  # MARKER
+    # print(os.listdir('.'))  # MARKER
+    # print(FILES)  # MARKER
 
     """Adds extension as a new item in the list if it does not exist"""
     for file in os.listdir('.'):
-        filenames = file.split('.')
-        if filenames[1] in extensions:
+        filename = file.split('.')
+        try:
+            os.mkdir(filename[1])
+        except FileExistsError:
+            shutil.move(file, filename[1])
+        except IndexError:
             continue
-        else:
-            extensions.append(filenames[1])
-
-    create_directory(extensions)
-
-
-def create_directory(extensions):
-    """Create a directory for each item in the list"""
-    for extension in extensions:
-        os.mkdir(extension)
 
 
 main()
